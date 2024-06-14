963fe949ef5499bc62afaf1788a5d7f9df56d45d
Selva Priya Sankar <selvapriya.sankar@sky.uk>
2024-06-14 14:27:14 +0530

    c2

--- JFrame1.java
+++ JFrame1.java
@@ -212,7 +212,7 @@ public class JFrame1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    //private javax.swing.JButton jButton1;
     //private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
